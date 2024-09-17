ada1afd491ea4c4faa8eee9ae270748c086bd5c0
Artem <baza.a1@bk.ru>
2024-09-17 15:59:08 +0300

    Создал модель продукта с названием, модель продукта и дата выхода на рынок

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название",
+        help_text="Укажите название"
+    )
+    product_model = models.CharField(
+        max_length=250,
+        verbose_name="Модель",
+        help_text="Укажите модель продукта"
+    )
+    create_date = models.DateTimeField(
+        verbose_name="Дата выхода на рынок",
+        help_text="Укажите дату выхода продукта на рынок"
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.product_model}) - {self.create_date}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
