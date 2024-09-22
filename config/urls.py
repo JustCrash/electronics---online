748cb06d4084f2b08206b1bd0ef67f4ecaa9db15
Artem <baza.a1@bk.ru>
2024-09-23 00:21:47 +0300

    Добавил урл путь для продукт

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls', namespace='users')),
     path('', include('company.urls', namespace='company')),
+    path('', include('product.urls', namespace='product')),
     # swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
