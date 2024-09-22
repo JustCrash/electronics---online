5b1c6eff697c8a799be7d1add882540ccc8df7b5
Artem <baza.a1@bk.ru>
2024-09-23 00:00:41 +0300

    Создал админку для company и supplier

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from company.models import Supplier, Company
+
+
+def clear_debt(queryset):
+    for obj in queryset:
+        obj.debt = 0
+        obj.save()
+
+
+clear_debt.short_description = "Очистить задолженность"
+
+fields_display = [
+    'name_supplier',
+    'debt',
+    'company_customer',
+    'company_supplier',
+    'owner',
+]
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    """
+    Интерфейс администратора для поставщика.
+    """
+    list_display = fields_display
+    actions = [clear_debt]
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    """
+    Интерфейс администрирования компании для администратора.
+    """
+    list_display = ('id', 'name', 'email', 'country', 'city', 'type_company', 'number_home', 'street',
+                    'owner')
+    search_fields = ('city',)
