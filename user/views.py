53aa316747a19474f536fd676c8913db506aa930
Artem <baza.a1@bk.ru>
2024-09-22 21:25:00 +0300

    User view set

--- user/views.py
+++ user/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from user.models import User
+from user.paginators import UsersPaginator
+from user.permissions import IsUser
+from user.serializers import UserSerializer, UserListSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    User view set.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = UsersPaginator
+
+    def get_permissions(self):
+        """
+        Проверка доступа пользователя.
+        """
+        if self.action == "create":
+            self.permission_classes = [AllowAny]
+            self.serializer_class = UserSerializer
+        if self.action in ["list"]:
+            self.permission_classes = [IsAuthenticated]
+            self.serializer_class = UserListSerializer
+        if self.action in ["retrieve"]:
+            self.permission_classes = [IsAuthenticated, IsUser]
+            self.serializer_class = UserSerializer
+        if self.action in ["update", "partial_update", "destroy"]:
+            self.permission_classes = [IsAuthenticated, IsUser]
+            self.serializer_class = UserSerializer
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        """
+        Хэширует пароль, созданный при регистрации.
+        """
+        instance = serializer.save(is_active=True)
+        instance.set_password(instance.password)
+        instance.save()
+
+    def perform_update(self, serializer):
+        """
+        Хэширует отредактированный пароль.
+        """
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
