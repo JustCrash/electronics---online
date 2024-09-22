e0561a92a59f5dd9241b400e87e991cf8f7c9da3
Artem <baza.a1@bk.ru>
2024-09-22 21:50:33 +0300

    Админка

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import Users
+
+
+@admin.register(Users)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Пользовательский интерфейс администрирования для администратора.
+    """
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff',
+                    'is_superuser')
