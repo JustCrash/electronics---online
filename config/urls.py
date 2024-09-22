e7a66b38b526894ae1a00d2f857656ca5ef644a3
Artem <baza.a1@bk.ru>
2024-09-22 21:34:28 +0300

    Подредактировал

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.Users', namespace='users')),
+    path('', include('user.urls', namespace='users')),
     # swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
